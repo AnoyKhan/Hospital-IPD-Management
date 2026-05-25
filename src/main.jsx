1ade4d0455126c01ecd0830409a6d3eb834a62ae
Anoy Khan <anoykhan14@gmail.com>
2026-05-25 16:23:48 +0600

    Use HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -16,9 +16,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ToastProvider>
         <AuthProvider>
           <AppProvider>
-            <BrowserRouter basename="/Hospital-IPD-Management/">
+            <HashRouter>
               <App />
-            </BrowserRouter>
+            </HashRouter>
           </AppProvider>
         </AuthProvider>
       </ToastProvider>
